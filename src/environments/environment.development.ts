42a408035c12be72ec265ba6cfa1d4edc02f36eb
klavzars <klavzar.simon@gmail.com>
2024-03-13 17:47:14 +0100

    env variables fix

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  apiUrl:
-    'https://ng-recipe-app-d9eda-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-  signupUrl:
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDfxwHJYk0bMyBHG1z6WqGFgb-j6tIocuA',
-  singinUrl:
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDfxwHJYk0bMyBHG1z6WqGFgb-j6tIocuA',
-};
